ff57d11525165ebaa0cb0dc212804abf95b468a2
codewithshamim <shamimislamonline@gmail.com>
2023-02-27 20:56:17 +0600

    updated

--- Leetcode/Easy/5.Binary-search.js
+++ Leetcode/Easy/5.Binary-search.js
@@ -1,10 +1,20 @@
 var search = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === target) {
-      return i;
+  const sortedArray = nums.sort((a, b) => a - b);
+  let start = 0;
+  let end = sortedArray.length - 1;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    const midValue = sortedArray[mid];
+
+    if (target === midValue) return nums.indexOf(midValue);
+    else if (midValue > target) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
     }
   }
   return -1;
 };
 
-console.log(search([1, 2, 3], 4));
+console.log(search([1, 2, 3, 4], 1));
