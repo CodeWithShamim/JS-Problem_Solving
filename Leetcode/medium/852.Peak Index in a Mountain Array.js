468c77fe36f711adf0d6f529d2561846d528a88b
codewithshamim <shamimislamonline@gmail.com>
2023-03-06 21:33:36 +0600

    solved

---
+++ Leetcode/medium/852.Peak Index in a Mountain Array.js
@@ -0,0 +1,21 @@
+var peakIndexInMountainArray = function (arr) {
+  let start = 0;
+  let end = arr.length - 1;
+  let mid;
+
+  while (start <= end) {
+    mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] < arr[mid + 1]) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+
+  if (arr[mid] < arr[mid + 1]) return mid + 1;
+  return mid;
+};
+
+console.log(peakIndexInMountainArray([0, 2, 1, 0]));
+console.log(peakIndexInMountainArray([0, 1, 2, 3, 4, 5, 6, 7, 10, 5, 2]));
