5dbb58636e872c69865ee976f72cfc06b2a9d41d
codewithshamim <shamimislamonline@gmail.com>
2023-03-09 20:37:41 +0600

    Solved

---
+++ Leetcode/medium/75.Sort colors.js
@@ -0,0 +1,15 @@
+var sortColors = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i; j < nums.length; j++) {
+      if (nums[i] > nums[j]) {
+        const temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+      }
+    }
+  }
+
+  return nums;
+};
+
+console.log(sortColors([2, 0, 2, 1, 1, 0]));
